ab281253013ded2604b1ba481a64f0a787c6d5a1
apfp77 <apfp77@naver.com>
2025-08-07 00:18:24 +0900

    feat: 테블릿 화면에서 모바일 헤더 보이도록 변경

--- components/common/MobileHeader.tsx
+++ components/common/MobileHeader.tsx
@@ -8,7 +8,7 @@ const MobileHeader = () => {
   const { unreadCount } = useNotificationStore();
 
   return (
-    <header className="fixed top-0 left-0 right-0 z-10 flex items-center justify-between h-14 px-4 bg-white dark:bg-black border-b border-gray-200 dark:border-gray-800 md:hidden">
+    <header className="fixed top-0 left-0 right-0 z-10 flex items-center justify-between h-14 px-4 bg-white dark:bg-black border-b border-gray-200 dark:border-gray-800 xl:hidden">
       <Link href="/" className="text-xl font-bold">
         PIKU
       </Link>
