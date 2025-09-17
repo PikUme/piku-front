557b28b906151822b4d63402745239a21618c523
apfp77 <apfp77@naver.com>
2025-09-17 23:15:43 +0900

    feat: 모바일 헤더 piku -> pikume로 변경

--- components/common/MobileHeader.tsx
+++ components/common/MobileHeader.tsx
@@ -10,7 +10,7 @@ const MobileHeader = () => {
   return (
     <header className="fixed top-0 left-0 right-0 z-10 flex items-center justify-between h-14 px-4 bg-white dark:bg-black border-b border-gray-200 dark:border-gray-800 xl:hidden">
       <Link href="/" className="text-xl font-bold">
-        PIKU
+        PikUme
       </Link>
       <Link href="/notifications" className="relative p-1">
         <Bell className="w-6 h-6" />
