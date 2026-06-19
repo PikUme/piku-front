3934c9a42fe78fd3e5edd2b6f34f7b5ab5668428
apfp77 <apfp77@naver.com>
2026-06-19 20:26:01 +0900

    feat: google tag 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -130,7 +130,31 @@ export default function RootLayout({
       lang="ko"
       className={`${geistSans.variable} ${geistMono.variable}`}
     >
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PZBS35WR');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body className="antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PZBS35WR"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <ReactQueryProvider>
           <LayoutWrapper>{children}</LayoutWrapper>
         </ReactQueryProvider>
